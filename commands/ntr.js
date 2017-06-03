4218b198ddd1bde2c0f5ae2da57a2374261dd1a2
pbanj <pbanj@gotbrew.org>
2017-06-02 23:32:47 -0400

    lets try this again

---
+++ commands/ntr.js
@@ -0,0 +1,5 @@
+exports.run = function(msg) {
+	msg.delete()
+	msg.channel.send('looking to install NTRcfw? \nGo here: https://github.com/Nanquitas/BootNTR/releases\nBanner doesnt matter, Mode3 is only for extended memory games on the O3DS systems').then(msg => {
+	})
+}
