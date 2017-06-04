cc35dbfcaa96f4835588f18577b688a057ea2365
pbanj <pbanj@gotbrew.org>
2017-06-04 12:51:56 -0400

    added new "bang" script

---
+++ commands/bang.js
@@ -0,0 +1,32 @@
+exports.run = function(msg) {
+	msg.delete()
+	msg.channel.send(':point_right:').then(msg => {
+		setTimeout(() => {
+			msg.edit(':ok_hand:').then(msg => {
+				setTimeout(() => {
+					msg.edit(':point_right:        :ok_hand:').then(msg => {
+						setTimeout(() => {
+							msg.edit(':point_right:      :ok_hand:').then(msg => {
+								setTimeout(() => {
+									msg.edit(':point_right:    :ok_hand:').then(msg => {
+										setTimeout(() => {
+											msg.edit(':point_right:  :ok_hand:').then(msg => {
+												setTimeout(() => {
+													msg.edit(':point_right::ok_hand:').then(msg => {
+														setTimeout(() => {
+															msg.edit('<:bang:319482085670912001>')
+														}, 350)
+													})
+												}, 350)
+											})
+										}, 350)
+									})
+								}, 350)
+							})
+						}, 350)
+					})
+				}, 350)
+			})
+		}, 350)
+	})
+}
